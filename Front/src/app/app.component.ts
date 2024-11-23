e1a2e481bd1529b1de0867ee2e152dce38a423a6
Wesley <wesley.bento@capgemini.com>
2024-11-23 17:31:46 -0300

    Alteração angular

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatRoomComponent } from '../components/chat-room/chat-room.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChatRoomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Front/src/services/socket.service.ts
+++ Front/src/services/socket.service.ts
@@ -12,11 +12,18 @@ export class SocketService {
   connect() {
     this.ws = io('ws://localhost:3000');
   }
-
+  /** envia uma mensagem para o servidor
+   * @param eventName nome do evento que deseja enviar
+   * @param data objeto que deseja enviar
+   */
   sendMessage(eventName: string, data: any): void {
     this.ws.emit(eventName, data);
   }
 
+  /** Abre um "listener" / como se fosse um ouvinto
+   * @param eventName o Evento que precisa ouvir que será vindo do servidor
+   * @function callback seria uma Arrow function ()=>{} que será executada. (ex: transformar dados de acordo com o que foi recebido)
+   */
   on(eventName: string, callback: (data: any) => void) {
     this.ws.on(eventName, callback);
   }
